4741cd38cef9ca2bf838e1482780f5ffad95444e
suknna <suknna@foxmail.com>
2026-06-07 10:30:24 +0800

    feat(govirtctl): wire thin entry over govirtctl.Run

--- cmd/govirtctl/main.go
+++ cmd/govirtctl/main.go
@@ -1,11 +1,15 @@
+// Command govirtctl is the Govirta control-plane CLI. It submits resource
+// manifests to the master apiserver and reads objects back. All logic lives in
+// internal/govirtctl; this entry only wires os.Args/stdio/exit code.
 package main
 
 import (
-	"fmt"
+	"context"
+	"os"
 
-	"github.com/suknna/govirta/internal/version"
+	"github.com/suknna/govirta/internal/govirtctl"
 )
 
 func main() {
-	fmt.Println(version.String())
+	os.Exit(govirtctl.Run(context.Background(), os.Args[1:], os.Stdout, os.Stderr))
 }
